ec1da6ef9edf7a7f837034efd6f3879d2e1714ed
Philip Capel <pcapel@users.noreply.github.com>
2022-11-11 09:24:25 -0600

    Add download using :ftp
    Path considerations aside, this does work! Always inspect your variables
    kids. Took way to long to figure out that I was getting a login error
    because of a newline in the string from `IO.gets`....

--- lib/mix/tasks/restrict/download.ex
+++ lib/mix/tasks/restrict/download.ex
@@ -8,15 +8,29 @@ defmodule Mix.Tasks.Restrict.Download do
   @shortdoc "Task for downloading RE data"
   use Mix.Task
 
-  def run(binstring) do
-    file_locations = [
-      "pub/rebase/emboss_e.###",
-      "pub/rebase/emboss_s.###",
-      "pub/rebase/emboss_r.###",
-      "pub/rebase/bairoch.###"
-    ]
-    Enum.map(file_locations, fn loc -> "ftp://ftp.neb.com/" <> loc)
+  def run(_) do
+    pass = IO.gets("Email as password: ") |> String.trim()
+    host = "ftp.neb.com"
+    date = Date.utc_today()
+    term = "#{String.slice("#{date.year}", 3, 99)}#{date.month}"
 
-    IO.inspect(binstring)
+    :ftp.start()
+    {:ok, pid} = :ftp.start_service(host: host |> String.to_charlist())
+    :ok = :ftp.user(pid, 'anonymous', ~c(pass))
+
+    [
+      "pub/rebase/emboss_e.",
+      "pub/rebase/emboss_s.",
+      "pub/rebase/emboss_r.",
+      "pub/rebase/bairoch."
+    ]
+    |> Enum.map(fn p -> p <> term end)
+    |> Enum.map(fn p -> {p, p |> Path.basename() |> Path.rootname()} end)
+    |> Enum.map(fn {path, target_filename} ->
+      :ok = :ftp.recv(pid, ~c(#{path}), "downloads_" <> target_filename)
+    end)
+
+    :ftp.stop_service(pid)
+    :ftp.stop()
   end
 end
