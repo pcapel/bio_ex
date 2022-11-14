ce74859745c3555bc47b23f1c90f1329ddcb8ebe
Philip Capel <pcapel@users.noreply.github.com>
2022-11-14 08:07:36 -0600

    Fix recursion and simplify _digest, expand test cases
    The recursive binary pattern matching is slick. I think it's pretty fast
    too, but I'm not sure how to benchmark things just yet.

--- lib/restriction.ex
+++ lib/restriction.ex
@@ -19,113 +19,47 @@ defmodule Bio.Restriction do
   ## Examples
       "ttagatgagctctcgattagagt"
        |> Bio.Restriction.digest(Bio.Restriction.Enzymes.bsmbi)
-        cut_1: "7",
-        cut_2: "11",
-      ["ttagatgacgtctcga", "ttagagt"]
-
-  What I want is a recursive approach that will ultimately let me build up a
-  value, so I need to reduce over the recursive product, right?
-
-  I can use binary pattern matching to achieve some of this.
+      > ["ttagatgacgtctcga", "ttagagt"]
   """
   def digest(dna, enzyme) do
-    _digest(byte_size(dna), byte_size(enzyme.pattern), dna, enzyme)
-    |> List.delete_at(-1)
+    _digest("", dna, enzyme)
     |> List.flatten()
   end
 
   defp _digest(
-         dna_size,
-         pattern_size,
-         dna,
+         left,
+         right,
          %Bio.Restriction.Enzyme{
            pattern: pattern,
            cut_1: fst5,
            cut_2: _fst3,
-           cut_3: scd5,
+           cut_3: _scd5,
            cut_4: _scd3
          } = enzyme
-       )
-       when fst5 > 0 and scd5 == 0 do
+       ) do
+    current_size = byte_size(right)
+    p_size = byte_size(pattern)
+
     cond do
-      dna_size > pattern_size ->
-        0..(dna_size - pattern_size)
-        |> Enum.reduce([], fn offset, products ->
-          <<left::binary-size(offset), check::binary-size(pattern_size), rest::binary>> = dna
+      # This is the base case, we can't reduce the sequence further
+      current_size <= p_size or current_size <= fst5 ->
+        [left <> right]
 
-          cond do
-            pattern == check ->
-              cut_site = offset + fst5
-              <<left_product::binary-size(cut_site), right_product::binary>> = dna
+      current_size > p_size ->
+        cut_site = 1 + fst5
+        <<left_product::binary-size(cut_site), right_product::binary>> = right
+        <<uncut::binary-size(1), check::binary-size(p_size), remaining::binary>> = right
 
-              products
-              |> List.insert_at(-1, left_product)
-              |> List.insert_at(
-                -1,
-                _digest(byte_size(right_product), pattern_size, right_product, enzyme)
-              )
+        cond do
+          pattern == check and left <> left_product != "" ->
+            [
+              left <> left_product,
+              _digest("", right_product, enzyme)
+            ]
 
-            rest == "" ->
-              products
-              |> List.insert_at(-1, left <> check)
-
-            true ->
-              products
-          end
-        end)
-
-      dna_size <= pattern_size ->
-        dna
-    end
-  end
-
-  # Deal with cuts that end up behind the pattern
-  # These are tricky. I think that I can rely on the fact that the boundary
-  # should have nucleotides in order to digest. For example, if I have the
-  # pattern ggcc and it cuts back 1 then gatggcc would cut to ga tggcc
-  # but if I have aggcctag then I would get no match.
-  defp _digest(
-         dna_size,
-         pattern_size,
-         dna,
-         %Bio.Restriction.Enzyme{
-           pattern: pattern,
-           cut_1: fst5,
-           cut_2: _fst3,
-           cut_3: scd5,
-           cut_4: _scd3
-         } = enzyme
-       )
-       when fst5 < 0 and scd5 == 0 do
-    cond do
-      dna_size > pattern_size ->
-        0..(dna_size - pattern_size)
-        |> Enum.reduce([], fn offset, products ->
-          <<left::binary-size(offset), check::binary-size(pattern_size), rest::binary>> = dna
-
-          cond do
-            pattern == check and byte_size(left) > abs(fst5) ->
-              cut_site = offset + fst5
-              <<left_product::binary-size(cut_site), right_product::binary>> = dna
-
-              products
-              |> List.insert_at(-1, left_product)
-              |> List.insert_at(
-                -1,
-                _digest(byte_size(right_product), pattern_size, right_product, enzyme)
-              )
-
-            rest == "" ->
-              products
-              |> List.insert_at(-1, left <> check)
-
-            true ->
-              products
-          end
-        end)
-
-      dna_size <= pattern_size ->
-        dna
+          true ->
+            _digest(left <> uncut, check <> remaining, enzyme)
+        end
     end
   end
 end
--- test/restriction_test.exs
+++ test/restriction_test.exs
@@ -1,8 +1,10 @@
+# Assertions should be given as Actual == Expected
 defmodule RestrictionTest do
   use ExUnit.Case
   doctest Bio.Restriction
 
   @doc """
+  BsmBI
   pattern: cgtctc
   cut offset: 7
 
@@ -12,14 +14,39 @@ defmodule RestrictionTest do
   ttacgtctca ^ gtagc
   """
   test "digest works with bsmbi" do
+    initial = "ttacgtctcagtagc"
+
     output =
-      "ttacgtctcagtagc"
+      initial
       |> Bio.Restriction.digest(Bio.Restriction.Enzyme.bsmbi())
 
     assert output == ["ttacgtctca", "gtagc"]
+    assert List.to_string(output) == initial
   end
 
   @doc """
+  BsmBI - multi
+  pattern: cgtctc
+  cut offset: 7
+
+  tta cgtctc agtagctta cgtctc agtagc
+      123456 1         123456 1
+
+  ttacgtctca ^ gtagcttacgtctca ^ gtagc
+  """
+  test "digest works with multiple bsmbi" do
+    initial = "ttacgtctcagtagcttacgtctcagtagc"
+
+    output =
+      initial
+      |> Bio.Restriction.digest(Bio.Restriction.Enzyme.bsmbi())
+
+    assert List.to_string(output) == initial
+    assert output == ["ttacgtctca", "gtagcttacgtctca", "gtagc"]
+  end
+
+  @doc """
+  PaqCI
   pattern: cacctgc
   cut offset: 11
 
@@ -29,11 +56,37 @@ defmodule RestrictionTest do
   tagcctcttacgcacctgccctg ^ cgcttaggacgatagttgagt
   """
   test "digest works with paqci" do
+    initial = "tagcctcttacgcacctgccctgcgcttaggacgatagttgagt"
+
     output =
-      "tagcctcttacgcacctgccctgcgcttaggacgatagttgagt"
+      initial
       |> Bio.Restriction.digest(Bio.Restriction.Enzyme.paqci())
 
+    assert List.to_string(output) == initial
     assert output == ["tagcctcttacgcacctgccctg", "cgcttaggacgatagttgagt"]
+  end
+
+  @doc """
+  PaqCI - near end
+  pattern: cacctgc
+  cut offset: 11
+
+  tagcctcttacgcccgccctgcgcttaggacgatag cacctgc ttt
+                                       1234567 1234
+
+  tagcctcttacgcccgccctgcgcttaggacgatagcacctgcttt
+
+  We shouldn't cut because the enzyme wouldn't be able to.
+  """
+  test "digest works with paqci too close to end" do
+    initial = "tagcctcttacgcccgccctgcgcttaggacgatagcacctgcttt"
+
+    output =
+      initial
+      |> Bio.Restriction.digest(Bio.Restriction.Enzyme.paqci())
+
+    assert List.to_string(output) == initial
+    assert output == ["tagcctcttacgcccgccctgcgcttaggacgatagcacctgcttt"]
   end
 
   @doc """
@@ -46,10 +99,111 @@ defmodule RestrictionTest do
   tagcctcttac gggcccctgcgcttaggacgatagttgagt
   """
   test "digest works with aoxi" do
+    starting = "tagcctcttacgggcccctgcgcttaggacgatagttgagt"
+
     output =
-      "tagcctcttacgggcccctgcgcttaggacgatagttgagt"
+      starting
       |> Bio.Restriction.digest(Bio.Restriction.Enzyme.aoxi())
 
+    assert List.to_string(output) == starting
     assert output == ["tagcctcttac", "gggcccctgcgcttaggacgatagttgagt"]
+  end
+
+  @doc """
+  pattern: ggcc,
+  cut offset: -1,
+
+  tagcctcttacg ggcccctgcgcttaggacgatagttgagttagcctcttacg ggcccctgcgcttaggacgatagttgagt
+             1 1234                                    1 1234
+
+  tagcctcttac ^ gggcccctgcgcttaggacgatagttgagttagcctcttac ^ gggcccctgcgcttaggacgatagttgagt
+  """
+  test "digest works with multiple sites aoxi" do
+    starting =
+      "tagcctcttacgggcccctgcgcttaggacgatagttgagttagcctcttacgggcccctgcgcttaggacgatagttgagt"
+
+    output =
+      starting
+      |> Bio.Restriction.digest(Bio.Restriction.Enzyme.aoxi())
+
+    assert List.to_string(output) == starting
+
+    assert output == [
+             "tagcctcttac",
+             "gggcccctgcgcttaggacgatagttgagttagcctcttac",
+             "gggcccctgcgcttaggacgatagttgagt"
+           ]
+  end
+
+  test "large sequence with aoxi" do
+    initial =
+      "ttgccgcggtcgtatacacaccggacggacacaagtatgtactaggaatggatcgaagcacgtgcactcatgctactcctttcacatacttggataacagcaatgttgaccacgttaagatctttacccgacctcgttgattcgtctcttcaagggaggagtccgaacaaattccgaactatttccttgcacattcgatcaggtcaatgcctcgtcccggctgaggagatagtcatgagatctcaaatgaatagggagggaaaatgagtgattagcgcgaaactatgtggtatcacagtttgtggagtctaaacgcagcagctgtttaactactcgctcaatcccgaaacgcggtgatagagcaacccattatgccgtgtgccgagggttacgtgtgccgcaattagcgtgggtaactatgacttttttgaggggaagctaagtccacctatacgaaagatgtgccgaacggctcaaacggcgtagagttcgtcttctcccgacggctaagcgaccctacattgggagactaagcggaagttgctcgaacagtcttgcaactatggtaaataattcttcacctcccagtccctcacgatcgattgttcctcgtgaagcagagctaatgtccccttactgctcgaccctactcgcagcgagcggcaatagcggggaactcgttaataagaggcagctcccacaatcaggtatggtctcacgctatgaatcggggcgacggactcgataatgggaacatgttgtgtgagggacgtaaactagatatcaggaagctaagtgtcctgatcggatcaatgattgttgaccaggtcagtctaattcgtatttgacaaaaacccgttggggcgtacctagtacctccccccaaccagctcccgacgaccgctacgctcgacattagaccgccgcgacctactgagatctatgggtctatacgggatttctgcgggactatcgacccgagcctcgccgctgaacctgctgcgggccttatcccgcatagacgtggcggaccagctcagcgctttggggcggttaaagacctgcacaaagatctggagctcgggctcagagggccccaggggattatagggtacttgcaaggtaaatacatgggaaagtgacgagcgggccttgtctcatacaaactacaagcgaacggatagatccctgtgtcccaaagtcacttcgggcgagtcgtagttaagattgagaccttaggcccgaataaggggctcctgaggccactctaataatgactaaagcccatcgtgatcataacaacttgtggactagtgcctgttaggatttgccacaagtgtcactcatggtcagcgcgggcgtgccgacgatcaacgaggagagcacttgtcagggtgctccatagggacggtatggaagacaacaaattcctcacagagaagttctcaaagatttttcttcaaagcgcccccggtggaatgagagatgattgggctctaat"
+
+    expected = [
+      "ttgccgcggtcgtatacacaccggacggacacaagtatgtactaggaatggatcgaagcacgtgcactcatgctactcctttcacatacttggataacagcaatgttgaccacgttaagatctttacccgacctcgttgattcgtctcttcaagggaggagtccgaacaaattccgaactatttccttgcacattcgatcaggtcaatgcctcgtcccggctgaggagatagtcatgagatctcaaatgaatagggagggaaaatgagtgattagcgcgaaactatgtggtatcacagtttgtggagtctaaacgcagcagctgtttaactactcgctcaatcccgaaacgcggtgatagagcaacccattatgccgtgtgccgagggttacgtgtgccgcaattagcgtgggtaactatgacttttttgaggggaagctaagtccacctatacgaaagatgtgccgaacggctcaaacggcgtagagttcgtcttctcccgacggctaagcgaccctacattgggagactaagcggaagttgctcgaacagtcttgcaactatggtaaataattcttcacctcccagtccctcacgatcgattgttcctcgtgaagcagagctaatgtccccttactgctcgaccctactcgcagcgagcggcaatagcggggaactcgttaataagaggcagctcccacaatcaggtatggtctcacgctatgaatcggggcgacggactcgataatgggaacatgttgtgtgagggacgtaaactagatatcaggaagctaagtgtcctgatcggatcaatgattgttgaccaggtcagtctaattcgtatttgacaaaaacccgttggggcgtacctagtacctccccccaaccagctcccgacgaccgctacgctcgacattagaccgccgcgacctactgagatctatgggtctatacgggatttctgcgggactatcgacccgagcctcgccgctgaacctgctgc",
+      "gggccttatcccgcatagacgtggcggaccagctcagcgctttggggcggttaaagacctgcacaaagatctggagctcgggctcaga",
+      "gggccccaggggattatagggtacttgcaaggtaaatacatgggaaagtgacgagc",
+      "gggccttgtctcatacaaactacaagcgaacggatagatccctgtgtcccaaagtcacttcgggcgagtcgtagttaagattgagacctt",
+      "aggcccgaataaggggctcctg",
+      "aggccactctaataatgactaaagcccatcgtgatcataacaacttgtggactagtgcctgttaggatttgccacaagtgtcactcatggtcagcgcgggcgtgccgacgatcaacgaggagagcacttgtcagggtgctccatagggacggtatggaagacaacaaattcctcacagagaagttctcaaagatttttcttcaaagcgcccccggtggaatgagagatgattgggctctaat"
+    ]
+
+    output =
+      initial
+      |> Bio.Restriction.digest(Bio.Restriction.Enzyme.aoxi())
+
+    assert List.to_string(output) == initial
+    assert output == expected
+  end
+
+  test "large sequence with bsmbi" do
+    output =
+      "cggaacatggcgtgccctaccggctataccgactgtctgagtcctctagggttcgtaatgacgccacccttagggtatacgggcagtaccacagtcaaatatttcaccagtctcctgtactcggaccttttaacgtcgatcaatgacagcaattttcgccatggtaggatttcccggtttgagcgtgagtcgattgccaggggcggagactaaagcgtttgctactcggcttggtctggatatcatttggagtgcgccaacttcagatccacctattgctgccgaaggagacggcgctcaagaacaccccatgttacccccgagatacgaggaagaagccggttagttggtttcagggcactgggaactctactgggctggtaagaggtatacaatatgtttgtgcctgcatgtataagccacagcctactctacaaagtggcgaagccacccacgggactgcgatcagggaggggttagccacagtccatgtattcagttgatacggatgtcagttagttggctcgcggtcgtcacagcggaagcatgcttcccgggctgggcttctgtagactctccatccggatgtgatctgatttttggtaaccacagatttcacagaggaggaagaacgggcaatatcctagcccgggaacagctgtctggttcgcgttcattaacacaccagctgcaaaacctcagaaagaattatgtgtctcacgaagcgtgtcgtagatgcctatcagcgtgcgcttagttggcacgggtggggccgactaggtactaggaacccttgcaatttcagggggacttagaaaggtccgtattggacagtagtccagaccctgagtgtgtatcaattggcttgcatagccattcaggtaatagctgtgcgtctccgaggactttctttctccggagtactggctctgtggatctcaactcatacacttgataataagtatccggcgcctgcgaagtctgccaacaactgtgtggcatttgccaagcggtcatgcttcggaatcacccgaagtttaaaatgcacttagaatgcctatcgttaacacagattcgcgaaggcgactgttggggaccgcgcttcaacgcaaggtactgtcgtcttcttggactacagagcgcgatacacacctgagtatagaaaaggtcaccgttttgtaacgcctggacctatagggtgacgggtttgcctcgactgtccgctttccactaaaaacagaaacttatcgtccatggtgacgtctgtttgtcaggtggttctggacgatcgccctgggaggattcaaatagttttgatctaccgcctcgtcggaaaccggtaccagagctaatgacaaattacggcccacccctgaataagtggtgtgaacggcctgcccgttgagtaacccccgcgtgagcatttgtgacgtctgaagtcgctcaaccgcgactacgtttattacacgggtataatttactgctcgcacatgtaaacggt"
+      |> Bio.Restriction.digest(Bio.Restriction.Enzyme.bsmbi())
+
+    expected = [
+      "cggaacatggcgtgccctaccggctataccgactgtctgagtcctctagggttcgtaatgacgccacccttagggtatacgggcagtaccacagtcaaatatttcaccagtctcctgtactcggaccttttaacgtcgatcaatgacagcaattttcgccatggtaggatttcccggtttgagcgtgagtcgattgccaggggcggagactaaagcgtttgctactcggcttggtctggatatcatttggagtgcgccaacttcagatccacctattgctgccgaaggagacggcgctcaagaacaccccatgttacccccgagatacgaggaagaagccggttagttggtttcagggcactgggaactctactgggctggtaagaggtatacaatatgtttgtgcctgcatgtataagccacagcctactctacaaagtggcgaagccacccacgggactgcgatcagggaggggttagccacagtccatgtattcagttgatacggatgtcagttagttggctcgcggtcgtcacagcggaagcatgcttcccgggctgggcttctgtagactctccatccggatgtgatctgatttttggtaaccacagatttcacagaggaggaagaacgggcaatatcctagcccgggaacagctgtctggttcgcgttcattaacacaccagctgcaaaacctcagaaagaattatgtgtctcacgaagcgtgtcgtagatgcctatcagcgtgcgcttagttggcacgggtggggccgactaggtactaggaacccttgcaatttcagggggacttagaaaggtccgtattggacagtagtccagaccctgagtgtgtatcaattggcttgcatagccattcaggtaatagctgtgcgtctcc",
+      "gaggactttctttctccggagtactggctctgtggatctcaactcatacacttgataataagtatccggcgcctgcgaagtctgccaacaactgtgtggcatttgccaagcggtcatgcttcggaatcacccgaagtttaaaatgcacttagaatgcctatcgttaacacagattcgcgaaggcgactgttggggaccgcgcttcaacgcaaggtactgtcgtcttcttggactacagagcgcgatacacacctgagtatagaaaaggtcaccgttttgtaacgcctggacctatagggtgacgggtttgcctcgactgtccgctttccactaaaaacagaaacttatcgtccatggtgacgtctgtttgtcaggtggttctggacgatcgccctgggaggattcaaatagttttgatctaccgcctcgtcggaaaccggtaccagagctaatgacaaattacggcccacccctgaataagtggtgtgaacggcctgcccgttgagtaacccccgcgtgagcatttgtgacgtctgaagtcgctcaaccgcgactacgtttattacacgggtataatttactgctcgcacatgtaaacggt"
+    ]
+
+    assert output == expected
+  end
+
+  test "large sequence with asi256i" do
+    output =
+      "cgagcgcttcgtaagtcggacgaaagaaaaaggtgatcaatagcatatcgtcttctgggaatccaccgtagccgatgctgtagcagtaaggtttgctacccattcgggtaaggccgctttgcaagttctttttggtacgcatgccgcgtggtaagggtgtcacagtgcacacattctcccatctaagtagttctatagaaaaacggttgggactagtaaatactgtagacaagtgacttatacaatcgtgccaggacgctgtgggtacgattcagttcgatctcttatacactcgtgcaatgtcgatcaattaagtaaaacaagagacttttagtgcatctttgggatcggtaagctgtcgtaagtacattttgcgcaatcttggtatatcccctcacgccgtccgatacgactacgcatgctgacgttagcttgttggagatggtgaaaaaaggtctgttggaaggccgtggccagtcgcacaacatcatataagattctccgtttatgttgtgtagagtcacatatattccggcggtcgcccaacgatacgcctacagaatgtatggataggctactggagcgtaacgttgccttcccctagttccgatatagcgtggtgatctaccagtgaaccctgtgtggctgcccttaaaggaaagcttgctgtcaccacaacggggtaacgtctatactcgcgatcgacctctaggttgagatcacagagcgacgcagcggcctaggcattggcaccctaggtggtagtctttccgtataactccctcaggtagaacaggcatttagccccacgccccggctgacctcaccgatgcgttcgacttttcgggtatacatgcggccgagcaccgagtgtttcttggaagtcccgactcaggtaccgtctgtagccctgaatgtactgaagccagaacatttatttgaggcaaattcctaaaaagaagtgagaacccaacgttccctccctgagcgtggccagggatagaactaaaagtctccatgagatatttccaggttcttgtaccctccatatagtattttccctgctctatggactcactccgtaattcccgccggccgctcctttgttccctcctgatactcagtccatcctgcctagccgactgcgcgaaacgaagctaaagcgtcgtgctagagcgagcttggaatcgaatcagggcataggtgagttcaatcggggaaccaccagcaccgcacttgacatccctgatttacgctgaagagttaatcaatgtgagagctggaactgctgaggtctttctgacttcataatgcctgcgctgtgacccaattgatattgtcgtcggaaagttattccgttccataccattggaactaagccagacatccgtttaattatgcggtctgagagagtactttcgctacgctctttctggttatcaggcgtacacgggaatcgtaatagattatcgtagtcaaagggacaacaaagggcacgctc"
+      |> Bio.Restriction.digest(Bio.Restriction.Enzyme.asi256i())
+
+    expected = [
+      "cgagcgcttcgtaagtcggacgaaagaaaaaggtg",
+      "atcaatagcatatcgtcttctgggaatccaccgtagccgatgctgtagcagtaaggtttgctacccattcgggtaaggccgctttgcaagttctttttggtacgcatgccgcgtggtaagggtgtcacagtgcacacattctcccatctaagtagttctatagaaaaacggttgggactagtaaatactgtagacaagtgacttatacaatcgtgccaggacgctgtgggtacgattcagttcg",
+      "atctcttatacactcgtgcaatgtcg",
+      "atcaattaagtaaaacaagagacttttagtgcatctttggg",
+      "atcggtaagctgtcgtaagtacattttgcgcaatcttggtatatcccctcacgccgtccgatacgactacgcatgctgacgttagcttgttggagatggtgaaaaaaggtctgttggaaggccgtggccagtcgcacaacatcatataagattctccgtttatgttgtgtagagtcacatatattccggcggtcgcccaacgatacgcctacagaatgtatggataggctactggagcgtaacgttgccttcccctagttccgatatagcgtggtg",
+      "atctaccagtgaaccctgtgtggctgcccttaaaggaaagcttgctgtcaccacaacggggtaacgtctatactcgcg",
+      "atcgacctctaggttgag",
+      "atcacagagcgacgcagcggcctaggcattggcaccctaggtggtagtctttccgtataactccctcaggtagaacaggcatttagccccacgccccggctgacctcaccgatgcgttcgacttttcgggtatacatgcggccgagcaccgagtgtttcttggaagtcccgactcaggtaccgtctgtagccctgaatgtactgaagccagaacatttatttgaggcaaattcctaaaaagaagtgagaacccaacgttccctccctgagcgtggccagggatagaactaaaagtctccatgagatatttccaggttcttgtaccctccatatagtattttccctgctctatggactcactccgtaattcccgccggccgctcctttgttccctcctgatactcagtccatcctgcctagccgactgcgcgaaacgaagctaaagcgtcgtgctagagcgagcttggaatcgaatcagggcataggtgagttcaatcggggaaccaccagcaccgcacttgacatccctgatttacgctgaagagttaatcaatgtgagagctggaactgctgaggtctttctgacttcataatgcctgcgctgtgacccaattgatattgtcgtcggaaagttattccgttccataccattggaactaagccagacatccgtttaattatgcggtctgagagagtactttcgctacgctctttctggttatcaggcgtacacgggaatcgtaatagattatcgtagtcaaagggacaacaaagggcacgctc"
+    ]
+
+    assert output == expected
+  end
+
+  test "large sequence with paqci" do
+    output =
+      "ccgacttgacagtgcgcaaagtcgtaagggtctagttaaatgtcgtcacctgcggcgatcaggatcgcaccatgtatatcgacgtattgtccgcgcgaatatcaactgatgagccttccagaagcgcatccacagatacgtccattcaggatcaccacacagtgggtccgtgtctccgtacacctgcttcacacacggtgatgatggttgctgcgtttcctagagcggtaccgatttatctgagttatgtcatgctcccaagtaagtgaaaacagtggctgttaaggtgcgggcctctatctgaaggcctgttgcgttctgcacatggacaagcccggtgcacacgccctacgattttcgtgatctgctcctcccgcccccctagcgaccggactactgtacgatcaaaaggctaatccggagattgcttgtatctgcggccgctagtgtcaacaaccgtacttcctgtccgtcgttggccgtttcagctaccccggtgataaagccagaatgggccagaccatgattagagcgtggaatttttccactaccagcgctaatatgacgcaggctcatttcgtgcacttcaaggcgtcaatacatctgatttggcaggctcgcccgtacgtgtctcaaacaatttcgctggtacctcctttccacgaagtatcatagtttgtgacgctccacctgctggtatcgcttttggtttccgtcatctggtagccaaatgcttcacctgcaccaaataatggtgggagttttggggtaaccaagcgctagatgtggcgtatgtcacaatgaggtttcacttacagtagccatagaggaaaaacttctagtagacctttcccctgttcagctcacaagggcctctttgggctcaggcctagtgcgcagttagcaggacactagcccaatgatctctaaaggggtcgcaagtggccaagttctacacatgtggggtaccagtctgttgccgtcacatatgacacttagcccttgtgactaccttctctgtgagccggtcattgagtccggccttatcgctaagaacgagctggtaatagcacctgctaagtctcattatgcttaatctggcagtgccatgttcagcaagtttaccaatatgctcatcacggagtaagctccgaatgacgcggaatgcccgtaggtcgcatcccaagtgtatttgcgagactgttaaacagtcgagcttaccaacgatggatctgacgtgaacacgtgatgagaggaacactgtaccaggagggcaacccggggagttgcatacggggaattatatcatcatgcacacaggacattcagcgtaacgtcggaccaggggagtaacgtatatggtgccatattgcggcgcacctgccttttcatcactgcgcagttccacaggctctctaattcctaggaatacacaaggccgtaaactaataactcccgcccctattagctcaagcgctgcccttttagatgcgtcgtataacctgcacgtgcagtgcgggagtagtctccatcgacacctgcacaggtat"
+      |> Bio.Restriction.digest(Bio.Restriction.Enzyme.paqci())
+
+    expected = [
+      "ccgacttgacagtgcgcaaagtcgtaagggtctagttaaatgtcgtcacctgcggcg",
+      "atcaggatcgcaccatgtatatcgacgtattgtccgcgcgaatatcaactgatgagccttccagaagcgcatccacagatacgtccattcaggatcaccacacagtgggtccgtgtctccgtacacctgcttca",
+      "cacacggtgatgatggttgctgcgtttcctagagcggtaccgatttatctgagttatgtcatgctcccaagtaagtgaaaacagtggctgttaaggtgcgggcctctatctgaaggcctgttgcgttctgcacatggacaagcccggtgcacacgccctacgattttcgtgatctgctcctcccgcccccctagcgaccggactactgtacgatcaaaaggctaatccggagattgcttgtatctgcggccgctagtgtcaacaaccgtacttcctgtccgtcgttggccgtttcagctaccccggtgataaagccagaatgggccagaccatgattagagcgtggaatttttccactaccagcgctaatatgacgcaggctcatttcgtgcacttcaaggcgtcaatacatctgatttggcaggctcgcccgtacgtgtctcaaacaatttcgctggtacctcctttccacgaagtatcatagtttgtgacgctccacctgctggt",
+      "atcgcttttggtttccgtcatctggtagccaaatgcttcacctgcacca",
+      "aataatggtgggagttttggggtaaccaagcgctagatgtggcgtatgtcacaatgaggtttcacttacagtagccatagaggaaaaacttctagtagacctttcccctgttcagctcacaagggcctctttgggctcaggcctagtgcgcagttagcaggacactagcccaatgatctctaaaggggtcgcaagtggccaagttctacacatgtggggtaccagtctgttgccgtcacatatgacacttagcccttgtgactaccttctctgtgagccggtcattgagtccggccttatcgctaagaacgagctggtaatagcacctgctaag",
+      "tctcattatgcttaatctggcagtgccatgttcagcaagtttaccaatatgctcatcacggagtaagctccgaatgacgcggaatgcccgtaggtcgcatcccaagtgtatttgcgagactgttaaacagtcgagcttaccaacgatggatctgacgtgaacacgtgatgagaggaacactgtaccaggagggcaacccggggagttgcatacggggaattatatcatcatgcacacaggacattcagcgtaacgtcggaccaggggagtaacgtatatggtgccatattgcggcgcacctgccttt",
+      "tcatcactgcgcagttccacaggctctctaattcctaggaatacacaaggccgtaaactaataactcccgcccctattagctcaagcgctgcccttttagatgcgtcgtataacctgcacgtgcagtgcgggagtagtctccatcgacacctgcacag",
+      "gtat"
+    ]
+
+    assert output == expected
   end
 end
